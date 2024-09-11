fa7416e91d82c5273ecff0c087b09779e9499a24
dasiloy <oyewoledamilare00@gmail.com>
2024-09-11 09:07:19 +0100

    pushing cahjnegs on app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MessagesModule } from './messages/messages.module';
+import * as morgan from 'morgan';
 
 @Module({
   imports: [MessagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(morgan('tiny')).forRoutes('*');
+  }
+}
